e56b8025b5a5e2a2c9c5d99f5e7134fd5799471a
Shuhui Luo <shuhui@aperture.finance>
2023-07-23 03:11:34 -0700

    Add ephemeral tick lens

---
+++ src/lens/EphemeralGetPopulatedTicksInRange.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import "./TickLens.sol";
+
+/// @notice A lens that fetches chunks of tick data for a Uniswap v3 pool without deployment
+/// @author Aperture Finance
+/// @dev The return data can be accessed externally by `eth_call` without a `to` address or internally by
+/// `address(new EphemeralGetPopulatedTicksInWord(pool, tickBitmapIndex)).code`, and decoded by
+/// `abi.decode(data, (PopulatedTick[]))`
+contract EphemeralGetPopulatedTicksInRange is TickLens {
+    constructor(V3PoolCallee pool, int24 tickLower, int24 tickUpper) {
+        PopulatedTick[] memory populatedTicks = getPopulatedTicksInRange(pool, tickLower, tickUpper);
+        bytes memory returnData = abi.encode(populatedTicks);
+        assembly ("memory-safe") {
+            revert(add(returnData, 0x20), mload(returnData))
+        }
+    }
+
+    /// @notice Get all the tick data for the populated ticks from tickLower to tickUpper
+    /// @param pool The address of the pool for which to fetch populated tick data
+    /// @param tickLower The lower tick boundary of the populated ticks to fetch
+    /// @param tickUpper The upper tick boundary of the populated ticks to fetch
+    /// @return populatedTicks An array of tick data for the given word in the tick bitmap
+    function getPopulatedTicksInRange(
+        V3PoolCallee pool,
+        int24 tickLower,
+        int24 tickUpper
+    ) public view returns (PopulatedTick[] memory populatedTicks) {
+        require(tickLower <= tickUpper);
+        int24 tickSpacing = pool.tickSpacing();
+        int16 wordPosLower;
+        int16 wordPosUpper;
+        {
+            int24 compressed = TickBitmap.compress(tickLower, tickSpacing);
+            wordPosLower = int16(compressed >> 8);
+            compressed = TickBitmap.compress(tickUpper, tickSpacing);
+            wordPosUpper = int16(compressed >> 8);
+        }
+        // calculate the number of populated ticks
+        uint256 numberOfPopulatedTicks;
+        for (int16 wordPos = wordPosLower; wordPos <= wordPosUpper; ++wordPos) {
+            numberOfPopulatedTicks += getNumberOfInitializedTicks(pool, wordPos);
+        }
+        // fetch populated tick data
+        populatedTicks = new PopulatedTick[](numberOfPopulatedTicks);
+        uint256 startIdx;
+        for (int16 wordPos = wordPosLower; wordPos <= wordPosUpper; ++wordPos) {
+            startIdx = populateTicksInWord(pool, wordPos, tickSpacing, populatedTicks, startIdx);
+        }
+    }
+}
