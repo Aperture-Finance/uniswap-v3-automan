1e83f5c08249550239c66e82d2492f00c90fee94
Shuhui Luo <shuhui@aperture.finance>
2023-08-13 05:55:36 -0700

    Refactor and test `RouterProxy`

--- src/RouterProxy.sol
+++ src/RouterProxy.sol
@@ -4,45 +4,45 @@ pragma solidity ^0.8.18;
 import "solady/src/utils/SafeTransferLib.sol";
 import {ERC20Callee} from "./libraries/ERC20Caller.sol";
 
-/// @dev This is a proxy contract that handles token approval/transfer and calls the router.
+/// @title Router Proxy
+/// @author Aperture Finance
+/// @dev This is a proxy contract that handles token approval/transfer and calls the router. The sigless `fallback` uses
+/// manual decoding of arguments in assembly to save gas.
 contract RouterProxy {
     using SafeTransferLib for address;
 
     fallback() external {
-        // `msg.data` is encoded as `abi.encodePacked(router, approvalTarget, tokenIn, tokenOut, amountIn, data)`.
+        /**
+            `msg.data` is encoded as `abi.encodePacked(router, approvalTarget, tokenIn, tokenOut, amountIn, data)`.
+            | Arg            | Offset   |
+            |----------------|----------|
+            | router         | [0, 20)  |
+            | approvalTarget | [20, 40) |
+            | tokenIn        | [40, 60) |
+            | tokenOut       | [60, 80) |
+            | amountIn       | [80, 112)|
+            | data.offset    | [112, )  |
+         */
         address router;
         address approvalTarget;
         address tokenIn;
         address tokenOut;
         uint256 amountIn;
-        bytes calldata data;
-        assembly ("memory-safe") {
+        assembly {
             router := shr(96, calldataload(0))
             approvalTarget := shr(96, calldataload(20))
             tokenIn := shr(96, calldataload(40))
             tokenOut := shr(96, calldataload(60))
             amountIn := calldataload(80)
-            data.length := sub(calldatasize(), 112)
-            data.offset := 112
         }
-        swapExactTokensForTokens(router, approvalTarget, tokenIn, tokenOut, amountIn, data);
-    }
-
-    function swapExactTokensForTokens(
-        address router,
-        address approvalTarget,
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        bytes calldata data
-    ) internal {
         tokenIn.safeTransferFrom(msg.sender, address(this), amountIn);
         tokenIn.safeApprove(approvalTarget, type(uint256).max);
         assembly ("memory-safe") {
             let fmp := mload(0x40)
-            calldatacopy(fmp, data.offset, data.length)
+            let dataLength := sub(calldatasize(), 112)
+            calldatacopy(fmp, 112, dataLength)
             // Ignore the return data unless an error occurs
-            if iszero(call(gas(), router, 0, fmp, data.length, 0, 0)) {
+            if iszero(call(gas(), router, 0, fmp, dataLength, 0, 0)) {
                 returndatacopy(0, 0, returndatasize())
                 // Bubble up the revert reason.
                 revert(0, returndatasize())
