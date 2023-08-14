d1b321d7a2004482cbe643fe7c1bb23638b028cd
Shuhui Luo <shuhui@aperture.finance>
2023-08-14 14:45:19 -0700

    Refactor `ERC20Caller` test

--- test/ERC20Caller.t.sol
+++ test/ERC20Caller.t.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../src/libraries/ERC20Caller.sol";
 
 /// @dev Test the ERC20Caller library.
 contract ERC20CallerTest is Test {
     address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    ERC20 internal testToken;
 
     function setUp() public {
         vm.createSelectFork("mainnet", 17000000);
+        testToken = new ERC20("TestToken", "TEST");
     }
 
     function test_TotalSupply() public {
@@ -17,47 +20,52 @@ contract ERC20CallerTest is Test {
         assertEq(IERC20(token).totalSupply(), ERC20Callee.wrap(token).totalSupply(), "totalSupply mismatch");
     }
 
-    function testFuzz_TotalSupply(address token) public {
-        (bool success, bytes memory data) = token.staticcall(abi.encodeWithSelector(IERC20.totalSupply.selector));
-        if (success && data.length == 32) {
-            assertEq(abi.decode(data, (uint256)), ERC20Callee.wrap(token).totalSupply(), "totalSupply mismatch");
-        }
+    function testFuzz_TotalSupply(uint256 amount) public {
+        address token = address(testToken);
+        deal(token, address(this), amount, true);
+        assertEq(IERC20(token).totalSupply(), ERC20Callee.wrap(token).totalSupply(), "totalSupply mismatch");
     }
 
-    function test_BalanceOf(address account) public {
+    function test_BalanceOf() public {
         address token = USDC;
-        assertEq(IERC20(token).balanceOf(account), ERC20Callee.wrap(token).balanceOf(account), "balanceOf mismatch");
-    }
-
-    function testFuzz_BalanceOf(address token, address account) public {
-        (bool success, bytes memory data) = token.staticcall(
-            abi.encodeWithSelector(IERC20.balanceOf.selector, account)
-        );
-        if (success && data.length == 32) {
-            assertEq(abi.decode(data, (uint256)), ERC20Callee.wrap(token).balanceOf(account), "balanceOf mismatch");
-        }
-    }
-
-    function test_Allowance(address owner, address spender) public {
-        address token = USDC;
+        deal(token, address(this), 1000);
         assertEq(
-            IERC20(token).allowance(owner, spender),
-            ERC20Callee.wrap(token).allowance(owner, spender),
+            IERC20(token).balanceOf(address(this)),
+            ERC20Callee.wrap(token).balanceOf(address(this)),
+            "balanceOf mismatch"
+        );
+    }
+
+    function testFuzz_BalanceOf(uint256 amount) public {
+        address token = address(testToken);
+        deal(token, address(this), amount);
+        assertEq(
+            IERC20(token).balanceOf(address(this)),
+            ERC20Callee.wrap(token).balanceOf(address(this)),
+            "balanceOf mismatch"
+        );
+    }
+
+    function test_Allowance() public {
+        address token = USDC;
+        address spender = address(1);
+        IERC20(token).approve(spender, type(uint256).max);
+        assertEq(
+            IERC20(token).allowance(address(this), spender),
+            ERC20Callee.wrap(token).allowance(address(this), spender),
             "allowance mismatch"
         );
     }
 
-    function testFuzz_Allowance(address token, address owner, address spender) public {
-        (bool success, bytes memory data) = token.staticcall(
-            abi.encodeWithSelector(IERC20.allowance.selector, owner, spender)
+    function testFuzz_Allowance(address spender, uint256 amount) public {
+        vm.assume(spender != address(0) && spender != address(this));
+        address token = address(testToken);
+        IERC20(token).approve(spender, amount);
+        assertEq(
+            IERC20(token).allowance(address(this), spender),
+            ERC20Callee.wrap(token).allowance(address(this), spender),
+            "allowance mismatch"
         );
-        if (success && data.length == 32) {
-            assertEq(
-                abi.decode(data, (uint256)),
-                ERC20Callee.wrap(token).allowance(owner, spender),
-                "allowance mismatch"
-            );
-        }
     }
 
     function test_Decimals() public {
