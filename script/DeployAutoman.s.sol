0ff1180b6d5f64b3da254778eb1ab6cd92bf299f
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2023-08-28 16:29:10 -0700

    Deploy `OptimalSwapRouter` (#24)

--- script/DeployAutoman.s.sol
+++ script/DeployAutoman.s.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
-import "../src/UniV3Automan.sol";
+import "../src/OptimalSwapRouter.sol";
 import "../src/RouterProxy.sol";
+import "../src/UniV3Automan.sol";
 
 contract DeployAutoman is Script {
     struct DeployParams {
@@ -15,7 +16,9 @@ contract DeployAutoman is Script {
 
     // https://github.com/pcaversaccio/create2deployer
     Create2Deployer internal constant create2deployer = Create2Deployer(0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2);
-    bytes32 internal constant salt = 0x5e05bd683b7817ee2a6b1d3cd7d8a34df4ee1696cf39a2a08878c523116daae7;
+    bytes32 internal constant automanSalt = 0x5e05bd683b7817ee2a6b1d3cd7d8a34df4ee1696cf39a2a08878c523116daae7;
+    bytes32 internal constant optimalSwapSalt = 0x25d61a346cb02fb86b96a0c62b1c222e04024550d908af19ba43cc43f767df7a;
+    bytes32 internal constant routerProxySalt = 0x862e41240a461e611c6c023e3cf74c29b2ab80b8e2b2539de1b8b1f096922723;
 
     // https://book.getfoundry.sh/tutorials/best-practices#scripts
     function readInput(string memory input) internal view returns (string memory) {
@@ -49,39 +52,49 @@ contract DeployAutoman is Script {
         console2.log("Automan initCodeHash:");
         console2.logBytes32(initCodeHash);
         // Compute the address of the contract to be deployed
-        UniV3Automan automan = UniV3Automan(payable(create2deployer.computeAddress(salt, initCodeHash)));
+        UniV3Automan automan = UniV3Automan(payable(create2deployer.computeAddress(automanSalt, initCodeHash)));
 
-        if (address(automan).code.length != 0) {
-            console2.log("Automan already deployed at: %s", address(automan));
-            bytes32 routerSalt = 0x862e41240a461e611c6c023e3cf74c29b2ab80b8e2b2539de1b8b1f096922723;
-            RouterProxy routerProxy = RouterProxy(
-                create2deployer.computeAddress(routerSalt, keccak256(type(RouterProxy).creationCode))
+        if (address(automan).code.length == 0) {
+            // Deploy automan
+            create2deployer.deploy(0, automanSalt, initCode);
+
+            // Set up automan
+            automan.setFeeConfig(params.feeConfig);
+            address[] memory controllers = new address[](1);
+            controllers[0] = params.controller;
+            bool[] memory statuses = new bool[](1);
+            statuses[0] = true;
+            automan.setControllers(controllers, statuses);
+            // Transfer ownership to the owner
+            automan.transferOwnership(params.owner);
+
+            console2.log(
+                "UniV3Automan deployed at: %s with owner %s and controller %s",
+                address(automan),
+                automan.owner(),
+                address(params.controller)
             );
-            // Deploy routerProxy
-            create2deployer.deploy(0, routerSalt, type(RouterProxy).creationCode);
-            console2.log("RouterProxy deployed at: %s", address(routerProxy));
-            vm.stopBroadcast();
-            return;
         }
-        // Deploy automan
-        create2deployer.deploy(0, salt, initCode);
 
-        // Set up automan
-        automan.setFeeConfig(params.feeConfig);
-        address[] memory controllers = new address[](1);
-        controllers[0] = params.controller;
-        bool[] memory statuses = new bool[](1);
-        statuses[0] = true;
-        automan.setControllers(controllers, statuses);
-        // Transfer ownership to the owner
-        automan.transferOwnership(params.owner);
-
-        console2.log(
-            "UniV3Automan deployed at: %s with owner %s and controller %s",
-            address(automan),
-            automan.owner(),
-            address(params.controller)
+        RouterProxy routerProxy = RouterProxy(
+            create2deployer.computeAddress(routerProxySalt, keccak256(type(RouterProxy).creationCode))
         );
+        if (address(routerProxy).code.length == 0) {
+            // Deploy routerProxy
+            create2deployer.deploy(0, routerProxySalt, type(RouterProxy).creationCode);
+            console2.log("RouterProxy deployed at: %s", address(routerProxy));
+        }
+
+        initCode = bytes.concat(type(OptimalSwapRouter).creationCode, abi.encode(params.npm));
+        initCodeHash = keccak256(initCode);
+        OptimalSwapRouter optimalSwapRouter = OptimalSwapRouter(
+            payable(create2deployer.computeAddress(optimalSwapSalt, initCodeHash))
+        );
+        if (address(optimalSwapRouter).code.length == 0) {
+            // Deploy optimalSwapRouter
+            create2deployer.deploy(0, optimalSwapSalt, initCode);
+            console2.log("OptimalSwapRouter deployed at: %s", address(optimalSwapRouter));
+        }
 
         // Deployment completed.
         vm.stopBroadcast();
