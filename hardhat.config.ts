d022f2787a79d186dfbb3bd8cbb64ce1c4fbb774
gnarlycow <peiqianli@gmail.com>
2024-08-09 15:59:20 -0700

    Update hardhat viaIR config to true for typechain.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 4294967295
       },
-      viaIR: false,
+      viaIR: true,
       evmVersion: "paris",
       metadata: {
         bytecodeHash: "none"
